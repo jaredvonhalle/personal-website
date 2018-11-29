3a7419576fec1cbf3bbda69896970e96768f3552
Jared Von Halle <>
2018-11-29 10:54:37 +0530

    change to ticketing font and improve styling of monitor text

--- src/components/ComputerMonitor.js
+++ src/components/ComputerMonitor.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import '../css/Index.scss';
 
+/*import stsicky_red from '../images/sticky_red.svg';
+<div className="computer-monitor-stickies col-1">
+<img src={stsicky_red}></img>
+</div>
+*/
+
 class ComputerMonitor extends Component {
 
 	render() {
@@ -8,7 +14,17 @@ class ComputerMonitor extends Component {
             <div className="computer-monitor row">
                 <div className="computer-monitor-text col">
                     <div className="computer-monitor-text-name">Jared von Halle</div>
-                    <div className="computer-monitor-text-about">Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared. Description about Jared.</div>
+                    <ul className="computer-monitor-text-about">
+                        <li className="computer-monitor-text-about-line">
+                        PROFESSIONALLY: Passionate about software development, DevOps, project management, and all things computer science.
+                        </li>
+                        <li className="computer-monitor-text-about-line">
+                        PERSONALLY: A perfect Sunday is an active day spent outdoors, includes a Jets victory, and ends with a home cooked meal shared with friends.
+                        </li>
+                        <li className="computer-monitor-text-about-line">
+                        CURRENTLY: A nomad, taking 6 months off to travel around Asia with my wife, before moving to Colorado.
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
